35681f119d677710ffaf0abeaf3257b570574f2f
sebam2k4 <sebam2k4@gmail.com>
2017-11-13 23:08:19 +0000

    added data route - fetch db and convert to json

--- app.py
+++ app.py
@@ -77,5 +77,29 @@ def index():
                 return render_template('index.html', errors = errors)
     return render_template('index.html', errors=errors, results=results, success = success)
 
+@app.route("/data")
+def webpage_data():
+    """
+    A Flask view to serve project data from
+    MongoDB in JSON format.
+    """
+
+    # A constant (uppercase) that defines the record fields that we wish to retrieve
+    FIELDS = {
+        '_id': False, 'url': True, 'title': True,
+        'description': True
+    }
+
+    # Open a connection to MongoDB using a 'with' statement such that the
+    # connection will be closed as soon as we exit the 'with' statement
+    with pymongo.MongoClient(MONGODB_HOST, MONGODB_PORT) as connection:
+        # Define which collection we wish to access
+        collection = connection[DBS_NAME][COLLECTION_NAME]
+        # Retrieve a result set only with the fields defined in FIELDS
+        # and limit the results to 250
+        webpage_data = collection.find(projection=FIELDS, limit=250)
+        # Convert projects to a list in a JSON object and return the JSON data
+        return json.dumps(list(webpage_data))
+
 if __name__ == '__main__':
     app.run(debug='true')
