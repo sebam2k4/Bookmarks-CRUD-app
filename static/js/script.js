c38d80a69ff22ff3ba7345c2346ab91530725fb4
sebam2k4 <sebam2k4@gmail.com>
2017-11-15 22:50:40 +0000

    modified json output html and added error message

--- static/js/script.js
+++ static/js/script.js
@@ -25,20 +25,37 @@ function loadData() {
         var output = '';
 
         for (var i in data) {
-        output += '<a class="card" target="_blank" href="' + data[i].url + '">' +
-            '<div class="panel panel-primary">' +
-            '<div class="panel-heading"><h4>' + data[i].title + '</h4></div>' +
-            '<div class="panel-body">' +
-            '<p>' + data[i].description + '</p>' +
-            '<p>url: ' + data[i].url + '</p>' +
-            '</div>' +
-            '</div>' +
-            '</a>';
+        output += '<div class="row">' +
+                        '<div class="card-container col-xs-10 col-sm-11">' +
+                            '<a class="card" target="_blank" href="' + data[i].url + '">' +
+                                '<div class="panel panel-primary">' +
+                                    '<div class="panel-heading">' +
+                                        '<h4>' + data[i].title + '</h4>' +
+                                    '</div>' +
+                                    '<div class="panel-body">' +
+                                        '<p>' + data[i].description + '</p>' +
+                                        '<p>url: ' + data[i].url + '</p>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</a>' +
+                        '</div>' +
+                        '<div class="card-buttons col-xs-2 col-sm-1">' +
+                            '<button type="button" class="btn btn-default" id="remove-card" aria-label="Left Align" title="remove">' +
+                                '<span class="glyphicon glyphicon glyphicon-remove" aria-hidden="true"></span>' +
+                            '</button>' +
+                            '<button type="button" class="btn btn-default" id="edit-card" aria-label="Left Align" title="edit">' +
+                                '<span class="glyphicon glyphicon glyphicon-pencil" aria-hidden="true"></span>' +
+                            '</button>' +
+                        '</div>' +
+                    '</div>';
         }
         document.getElementById('output').innerHTML = output;
     } else if(this.status == 404) {
         console.log("Data not found (404)");
-        document.getElementById('output').innerHTML = "Data not not found (404)";
+        document.getElementById('output').innerHTML = "Data not found (404)";
+    } else if(this.status == 500) {
+        console.log("Internal Server Error (500)");
+        document.getElementById('output').innerHTML = "Internal Server Error (500)";
       }
     }
 
