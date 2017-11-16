cdadfaa2334f440d5a8340dcbed84f89056f385c
sebam2k4 <sebam2k4@gmail.com>
2017-11-16 00:00:48 +0000

    moved add_url funtion outside route and adjuster error display on page

--- app.py
+++ app.py
@@ -12,68 +12,74 @@ MONGODB_PORT = 27017
 DBS_NAME = 'client-database'
 COLLECTION_NAME = 'webpage_data'
 
+
+
+def add_url(url):
+    errors = []
+    try:
+        html_text = urlopen(url).read()
+    except:
+        error = "Unable to get URL. Make sure it's valid and try again."
+        print error
+        errors.append(error)
+        return {'error': errors}
+
+    # after successful url read, scrape urls for data:
+    if html_text:
+        soup = BeautifulSoup(html_text, "lxml")
+        site_title = soup.title.string
+        site_description = soup.html.head.find("meta", attrs={"name":"description"})
+        # need to create new dicionary for each new item/iteration to be able to append to list later
+        data = {
+            'url': url,
+            'description': site_description['content'] if site_description else "No description given",
+            'title': site_title if site_title else "No title given"
+        }
+
+        def mongo_connect():
+            try:
+                connection = pymongo.MongoClient('localhost', 27017)
+                print "Mongo is connected!"
+                return connection
+            except pymongo.errors.ConnectionFailure, e:
+                error = "Could not connect to MongoDB: %s" % e
+                print error
+                errors.append(error)
+                return {'error': errors}
+        
+        # insert data to database
+        try:
+            connection = mongo_connect() #connect to MongoDB
+            db = connection['client-database'] # db name
+            collection = db.webpage_data # db collectinon name
+            # add record to collection if it doesn't already exist
+            if collection.find_one({"url": url}) < 0:
+                collection.save(data) # insert scraped data to db
+                success = "Record Successfully Added!"
+                return {'success': success}
+            else:
+                error = "Record already exists in database!"
+                print error
+                errors.append(error)
+                return {'error': errors}
+        except:
+            error = "Unable to add record to database due to unrecognized error."
+            print error
+            errors.append(error)
+            return {'error': errors}
+
 # Flask routes:
 @app. route('/', methods=['GET', 'POST'])
 def index():
-    errors = []
-    #results = []
-    if request.method == "POST":
-        # get url that the user has entered
-        try:
-            url = request.form['url']
-            html_text = urlopen(url).read()
-        except:
-            error = "Unable to get URL. Make sure it's valid and try again."
-            print error
-            errors.append(error)
-            return render_template('index.html', errors = errors)
-
-        # after successful url read, scrape urls for data:
-        if html_text:
-            soup = BeautifulSoup(html_text, "lxml")
-            site_title = soup.title.string
-            site_description = soup.html.head.find("meta", attrs={"name":"description"})
-            # need to create new dicionary for each new item/iteration to be able to append to list later
-            data = {
-                'url': url,
-                'description': site_description['content'] if site_description else "No description given",
-                'title': site_title if site_title else "No title given"
-            }
-            # append scraped data to results list/ don't really need atm
-            #results.append(data)
-
-            def mongo_connect():
-                try:
-                    connection = pymongo.MongoClient('localhost', 27017)
-                    print "Mongo is connected!"
-                    return connection
-                except pymongo.errors.ConnectionFailure, e:
-                    error = "Could not connect to MongoDB: %s" % e
-                    print error
-                    errors.append(error)
-                    return render_template('index.html', errors = errors)
-            
-            # insert data to database
-            try:
-                connection = mongo_connect() #connect to MongoDB
-                db = connection['client-database'] # db name
-                collection = db.webpage_data # db collectinon name
-                # add record to collection if it doesn't already exist
-                if collection.find_one({"url": url}) < 0:
-                    collection.save(data) # insert scraped data to db
-                    success = "Record Successfully Added!"
-                    return render_template('index.html', success=success)
-                else:
-                    error = "Record already exists in database!"
-                    print error
-                    errors.append(error)
-                    return render_template('index.html', errors = errors)
-            except:
-                error = "Unable to add record to database due to unrecognized error."
-                print error
-                errors.append(error)
-                return render_template('index.html', errors = errors)
-    return render_template('index.html', errors=errors)
+    results = []
+    # get url that the user has entered
+    # Add url from form input name='url'
+    if request.method == "POST" and request.form['add-card']:
+        url = request.form['add-card']
+        results = add_url(url)
+        print results
+    # else if request.method == "POST" and request.form['remove-card']:
+    return render_template('index.html', results = results)
 
 @app.route("/data")
 def webpage_data():
