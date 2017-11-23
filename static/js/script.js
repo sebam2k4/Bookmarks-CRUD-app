d99082738fae5ab3f0aadcdabc6e78c564c608fc
sebam2k4 <sebam2k4@gmail.com>
2017-11-23 11:47:19 +0000

    General code cleanup, comments, added hide other buttons on card edit

--- static/js/script.js
+++ static/js/script.js
@@ -1,138 +1,4 @@
 $(document).ready(function() {
-    // EDIT CARD
-    $('#cards').on("click", '.editCardBtn', function(){
-        // disable links to prevent opening the card's link while in edit mode
-        $('a').on('click', function(element){
-            element.preventDefault();
-        })
-        // get the button's value (url)
-        var value = $(this).val();
-        //make a list of all cards
-        var card_list = document.getElementsByClassName('card-container')   // var card_list = $('.card-container)
-        //var card_list2 = card_list[0].getAttribute("data-identifier");      // and then // card_list[0].data('identifier') doesn't work for some reason //using vanilla js instead.
-        //console.log(card_list2)
-        //console.log(arr2.data('identifier'))
-        // get the data attribute for matching card (url)
-        var card_match_identifier = $(this).closest('.card-container').data('identifier'); 
-        // hide/show SAVE button
-        $('.saveCardBtn').hide();
-        $(this).next().fadeIn(300);
-        // activate card's editable content and add borders
-        for (var i = 0; i < card_list.length; i++) {
-            if (card_list[i].getAttribute("data-identifier") == value) {
-                // clear any active editable content first
-                $(card_list).find('.edit-h4').removeClass('active').attr('contenteditable','false');
-                $(card_list).find('.edit-p').removeClass('active').attr('contenteditable','false');
-                // activate editable content for corresponding card
-                $(card_list[i]).find('.edit-h4').addClass('active').attr('contenteditable','true');
-                $(card_list[i]).find('.edit-p').addClass('active').attr('contenteditable','true');
-            }
-        }        
-    });
-
-    // SAVE EDITED CARD
-    $('#cards').on("click", '.saveCardBtn', function(){
-        // remove click event to re-enable card links after save
-        $('a').off('click')
-        // hide save button after save
-        var value = $(this).val();
-        console.log(value)
-        //make a list of all cards
-        var card_list = document.getElementsByClassName('card-container')   // var card_list = $('.card-container)
-        console.log(card_list)
-        //var card_list2 = card_list[0].getAttribute("data-identifier");      // and then // card_list[0].data('identifier') doesn't work for some reason //using vanilla js instead.
-        //console.log(card_list2)
-        //console.log(arr2.data('identifier'))
-        // get the data attribute for matching card (url)
-        var card_match_identifier = $(this).closest('.card-container').data('identifier'); 
-        console.log(card_match_identifier)
-        // hide/show SAVE button
-        $('.saveCardBtn').hide();
-        $(this).next().fadeIn(300);
-        // activate card's editable content and add borders
-        var title = '';
-        var description = '';
-        for (var i = 0; i < card_list.length; i++) {
-            if (card_list[i].getAttribute("data-identifier") == value) {
-                $(card_list[i]).find('.edit-h4').removeClass('active').attr('contenteditable','false');
-                $(card_list[i]).find('.edit-p').removeClass('active').attr('contenteditable','false');
-                title = $(card_list[i]).find('.edit-h4').text();
-                description = $(card_list[i]).find('.edit-p').text();
-            }
-        }
-
-        var input_url = value;
-        $.ajax({
-            url: '/update_card',
-            data: JSON.stringify({'url': input_url, 'title':title, 'description':description}),
-            contentType: 'application/json;charset=UTF-8',
-            type: 'POST',
-            success: function(response){
-                // output server error or success message:
-                showMessage(response);
-                
-            },
-            error: function(error){
-                console.log(error);
-            }
-        })
-        // reload data
-        .done(function() {
-            loadData();
-        });
-        $('.saveCardBtn').fadeOut(150);
-    });
-
-    // ADD CARD
-    $('#addUrlForm').on("submit", function(){
-        // prevent page reload on form submit (default behaviour)
-        event.preventDefault();
-        // capture url from user input
-        var input_url = $('#inputUrl').val();
-        $.ajax({
-            url: '/add_card',
-            data: $('form').serialize(),
-            type: 'POST',
-            success: function(response){
-                // output server error or success message:
-                showMessage(response);
-            },
-            error: function(error){
-                console.log(error);
-            }
-        })
-        // reload data
-        .done(function() {
-            loadData();
-        });
-    });
-
-    // REMOVE CARD
-    // .on method with delegated event needed for click event to work on future elements
-    // Needs to be used on static parent element
-    $('#cards').on("click", '.removeCardBtn', function(){
-        // capture value of clicked button (url)
-        var input_url = $(this).val();
-        console.log(input_url)
-        $.ajax({
-            url: '/remove_card',
-            data: JSON.stringify(input_url),
-            contentType: 'application/json;charset=UTF-8',
-            type: 'POST',
-            success: function(responseText){
-                // output server error or success message:
-                showMessage(responseText)
-            },
-            error: function(error){
-                console.log(error);
-            }
-        })
-        // reload data
-        .done(function() {
-            loadData();
-        });
-    });
-
 
     // LOAD DATA from server api
     function loadData() {
@@ -150,10 +16,10 @@ $(document).ready(function() {
                                     '<a class="card" target="_blank" href="' + reversed_data[i].url + '">' +
                                         '<div class="panel panel-primary">' +
                                             '<div class="panel-heading">' +
-                                                '<h4 class="edit-h4" contenteditable="false">' + reversed_data[i].title + '</h4>' +
+                                                '<h4 class="edit-h4">' + reversed_data[i].title + '</h4>' +
                                             '</div>' +
                                             '<div class="panel-body">' +
-                                                '<p class="edit-p" contenteditable="false">' + reversed_data[i].description + '</p>' +
+                                                '<p class="edit-p">' + reversed_data[i].description + '</p>' +
                                                 '<p class="text-muted small url">' + reversed_data[i].url + '</p>' +
                                             '</div>' +
                                         '</div>' +
@@ -171,7 +37,6 @@ $(document).ready(function() {
                                     '</button>' +
                                 '</div>' +
                             '</div>';
-        
                 }
                 $('#cards').html(output);
             },
@@ -181,8 +46,181 @@ $(document).ready(function() {
         });
     };
 
+    // ADD CARD
+    $('#addUrlForm').on("submit", function(){
+        // prevent page reload on form submit (default behaviour)
+        event.preventDefault();
+        // capture url from user input
+        var input_url = $('#inputUrl').val();
+        $.ajax({
+            url: '/add_card',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response){
+                // output server error or success message:
+                showServerMessage(response);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        })
+        .done(function() {
+            // reload data
+            loadData();
+        });
+    });
+
+    // REMOVE CARD
+    // .on method with delegated event needed for click event to work on created html elements by loadData()
+    // Needs to be used on static parent element
+    $('#cards').on("click", '.removeCardBtn', function(){
+        // capture value of clicked button (url)
+        var input_url = $(this).val();
+        console.log(input_url)
+        $.ajax({
+            url: '/remove_card',
+            data: JSON.stringify(input_url),
+            contentType: 'application/json;charset=UTF-8',
+            type: 'POST',
+            success: function(responseText){
+                // output server error or success message:
+                showServerMessage(responseText)
+            },
+            error: function(error){
+                console.log(error);
+            }
+        })
+        .done(function() {
+            // reload data
+            loadData();
+        });
+    });
+
+
+    // EDIT CARD
+    // clean this up - looks really messy and code could possibly be improved.
+    // Read up jquery docs and investigate some ways to imporove and simplify
+    $('#cards').on("click", '.editCardBtn', function(){
+        // disable links to prevent opening the card's link while in edit mode
+        $('a').on('click', function(element){
+            element.preventDefault();
+        })
+        // get the button's value (url)
+        var value = $(this).val();
+        //make a list of all cards
+        var card_list = $('.card-container');
+        // get the data attribute for matching card (url)
+        var card_match_identifier = $(this).closest('.card-container').data('identifier');
+        // hide SAVE button for matching card card
+        $('.saveCardBtn').hide();
+        // hide buttons for other cards when in card edit mode
+        $('.editCardBtn').not(this).closest('.card-buttons').toggle();
+
+        // initialize variables
+        var all_titles = '';
+        var all_descriptions = '';
+        var title = '';
+        var description = '';
+
+        // iterate through every card
+        //var original_title_text = '';
+        for (var i = 0; i < card_list.length; i++) {
+            // get 2 seperate lists for all titles and descriptions of all cards
+            all_titles = $(card_list).find('.edit-h4');
+            all_descriptions = $(card_list).find('.edit-p');
+            title = $(card_list[i]).find('.edit-h4');
+            description = $(card_list[i]).find('.edit-p');
+            var original_title_text = title.text(); //how to compare this to new text?
+
+            // Match edit button with its corresponding card
+            if (card_list.eq(i).data('identifier') == value) { // card_list[0].getAttribute("data-identifier");
+                
+                // activate editable content for corresponding card
+                if (title.attr('contenteditable')) {
+                    // disable active editable content on 2nd button click
+                    title.removeClass('active').removeAttr('contenteditable','true');
+                    description.removeClass('active').removeAttr('contenteditable','true');
+                    // remove click event to re-enable card links after save
+                    $('a').off('click');
+                    $('.saveCardBtn').hide();
+                    console.log(original_title_text)
+                    title = original_title_text;
+                    $(".card-buttons").show();
+                } else {
+                    // show save button
+                    $(this).next().fadeIn(300);
+                    // clear any active editable content first
+                    all_titles.removeClass('active').removeAttr('contenteditable','false');
+                    all_descriptions.removeClass('active').removeAttr('contenteditable','false');
+                    // activate editable content for corresponding card
+                    title.addClass('active').attr('contenteditable','true');
+                    description.addClass('active').attr('contenteditable','true');
+                    // focus workaround for contentEditable attribute (places caret at begining of line though :( 
+                    setTimeout(function() {
+                        title.focus();
+                    }, 0);
+                }
+            }
+        } 
+    });
+
+    // SAVE EDITED CARD
+    // clean this up - looks really messy and code could possibly be improved.
+    // Read up jquery docs and investigate some ways to imporove and simplify
+    $('#cards').on("click", '.saveCardBtn', function(){
+        // remove click event to re-enable card links after save
+        $('a').off('click')
+        
+        var value = $(this).val();
+        //make a list of all cards
+        var card_list = $('.card-container');
+        // get the data attribute for matching card (url)
+        var card_match_identifier = $(this).closest('.card-container').data('identifier');
+        // initialize variables
+        var title = '';
+        var description = '';
+        var title_text = '';
+        var description_text = '';
+
+        // iterate through every card
+        for (var i = 0; i < card_list.length; i++) {
+            title = $(card_list[i]).find('.edit-h4');
+            description = $(card_list[i]).find('.edit-p');
+
+            // Match save button with its corresponding card
+            if (card_list.eq(i).data('identifier') == value) {
+                // Save coresponding card's edited content and remove borders & disable editable content.
+                title.removeClass('active').attr('contenteditable','false');
+                description.removeClass('active').attr('contenteditable','false');
+                title_text = title.text();
+                description_text = description.text();
+            }
+        }
+        // Save edited content to db
+        var input_url = value;
+        $.ajax({
+            url: '/update_card',
+            data: JSON.stringify({'url': input_url, 'title':title_text, 'description':description_text}),
+            contentType: 'application/json;charset=UTF-8',
+            type: 'POST',
+            success: function(response){
+                // output server error or success message:
+                showServerMessage(response);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        })
+        .done(function() {
+            // don't need to reload data here after edit save. The front-end is updated already
+            // loadData();
+            // hide save button after save
+            $('.saveCardBtn').fadeOut(150);
+        });
+    });
+
     // show error or success messages from the server api
-    function showMessage(response) {
+    function showServerMessage(response) {
     var output = '';
         if (response.results.hasOwnProperty('error')) {
         var errors = (response.results.error);
